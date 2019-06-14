af8c1600ff7c13449372121be03b94bf25f16792
Glerison <Glerisonlima@gmail.com>
2019-06-14 17:47:07 -0300

    segundo dia

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,4 +1,8 @@
 const Post = require('../models/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
+
 
 module.exports = {
     async index (req, res){
@@ -7,15 +11,28 @@ module.exports = {
         return res.json(posts)
     },
     async store (req, res){
+        console.log(req.body.author)
         const {author, place, description, hashtags} = req.body;
         const {filename: image} = req.file;
 
-        const post = await Post.create({
-            author,
+        const [name] = image.split('.')
+        const fileName = `${name}.jpg`
+
+        await sharp(req.file.path) 
+            .resize(500)
+            .jpeg({ quality: 70 })
+            .toFile(
+                path.resolve(req.file.destination, 'resized', fileName)
+            )
+
+        fs.unlinkSync(req.file.path)
+ 
+        const post = await Post.create({            
             place,
+            author,
             description,
             hashtags,
-            image
+            image: fileName,
         });
 
         return res.json(post)
